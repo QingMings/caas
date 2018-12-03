93ea2af47b8252ab053c0ee6bbf5146f993ee3d7
QingMings <1821063757@qq.com>
2018-12-03 09:12:09 +0800

    添加底部状态栏

---
+++ src/components/bottomPane.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="bottomPane">
+aa
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  name: 'bottomPane',
+  data() {
+    return {}
+  }
+}
+</script>
+<style>
+
+</style>
